cd55263ca0be97966a8ac0faa969b90521c1d386
Kenny Burgher <kenny.burgher@gmail.com>
2023-06-09 10:43:06 -0700

    nav bar collapse effect

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -20,35 +20,48 @@ function NavBar({ logout }) {
 
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light fixed-top">
-      <div className="container">
+      <div className="container-fluid">
         <NavLink to="/" className="navbar-brand">
           Friender
         </NavLink>
-        {user ? (
-          <div className="navbar-nav mr-auto">
-          <NavLink to="/swipe" className="nav-link">
-              Swipe
-            </NavLink>
-            <NavLink to="/matches" className="nav-link">
-              Matches
-            </NavLink>
-            <NavLink to="/profile" className="nav-link">
-              Profile
-            </NavLink>
-            <NavLink to="/" className="nav-link" onClick={handleLogout}>
-              Log out
-            </NavLink>
-          </div>
-        ): (
-          <div className="navbar-nav mr-auto">
-            <NavLink to="/login" className="nav-link">
-              Login
-            </NavLink>
-            <NavLink to="/signup" className="nav-link">
-              Sign Up
-            </NavLink>
-          </div>
-        )}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          {user ? (
+            <div className="navbar-nav ms-auto">
+              <NavLink to="/swipe" className="nav-link">
+                Swipe
+              </NavLink>
+              <NavLink to="/matches" className="nav-link">
+                Matches
+              </NavLink>
+              <NavLink to="/profile" className="nav-link">
+                Profile
+              </NavLink>
+              <NavLink to="/" className="nav-link" onClick={handleLogout}>
+                Log out
+              </NavLink>
+            </div>
+          ) : (
+            <div className="navbar-nav ms-auto">
+              <NavLink to="/login" className="nav-link">
+                Login
+              </NavLink>
+              <NavLink to="/signup" className="nav-link">
+                Sign Up
+              </NavLink>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
